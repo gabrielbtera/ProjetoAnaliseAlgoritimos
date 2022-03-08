e90bf669fc8e475f66a9586a0d227495d9879f76
gabrielbtera <gabriel.jesus@dcomp.ufs.br>
2022-03-08 00:08:30 -0300

    implementacao do merge sort

--- 1 mergeSort/1mergeSort.py
+++ 1 mergeSort/1mergeSort.py
@@ -2,13 +2,6 @@ import sys
 
 from time import time
 
-# E = ['QOZJ7913219 34.699.211/9365-11 13822' for i in range(0, 60000)]
-# print(E)
-# arq = open('teste2.txt', 'w')
-# for i in E:
-#     arq.write(i+'\n')
-# inicio = time()
-# print(inicio)
 # def mergeSort(lista, inicio, fim):
   
 #   if (fim - inicio) > 1:
@@ -61,7 +54,8 @@ def primeiraParte(arquivo):
     numeroPrimeira = int(arquivo[0])
     conteiners = arquivo[1: numeroPrimeira + 1]
     for conteiner in range( numeroPrimeira):
-        conteiners[conteiner] = conteiners[conteiner].split(' ')
+        conteinerPronto = conteiners[conteiner].split(' ')
+        conteiners[conteiner] = [conteinerPronto[0], conteinerPronto[1], int(conteinerPronto[2])]
     return conteiners
     
 
@@ -69,13 +63,74 @@ def segundaParte(arquivo):
     numeroSegunda = int(arquivo[0]) + 1
     dicSegundo  = {}
     conteiners = arquivo[numeroSegunda + 1:]
-
     for conteiner in conteiners:
         splitConteiner = conteiner.split(' ')
         dicSegundo[splitConteiner[0]] = (splitConteiner[1], int(splitConteiner[2]))
-    
-    print(dicSegundo)
     return dicSegundo
+
+def percentuaDedoisnumeros(numero1, numero2):
+    if numero1 > numero2:
+        return round(((numero1 - numero2) / numero2) * 100), numero1 - numero2
+
+    return round(((numero2 - numero1) / numero1) * 100), numero2 -numero1
+
+
+def ordenaPeloCNPJEextraipercentual(listaPivo, dicAnalisado):
+    lista = []
+    lista2 = []
+    contador1 = 0
+    contador2 = 0
+    for conteinerPivot  in listaPivo:
+        conteinerAnalisado = dicAnalisado.get(conteinerPivot[0])
+        if (conteinerAnalisado):
+            percentualPeso, diferenca = percentuaDedoisnumeros(conteinerAnalisado[1], conteinerPivot[2])
+            if (conteinerAnalisado[0] != conteinerPivot[1]):
+                lista.append([conteinerPivot[0], conteinerAnalisado[0], conteinerAnalisado[1]])
+                contador1 += 1
+            elif (percentualPeso > 10):
+                lista2.append([conteinerPivot[0], conteinerAnalisado[0], percentualPeso, diferenca])
+                contador2 += 1
+
+    lista = lista + lista2
+    return lista, contador1, contador2
+
+
+
+def mergeSort(lista, inicio, fim):
+    if (fim - inicio) > 1:
+        meio = (fim + inicio) // 2
+        print('meio', meio)
+        mergeSort(lista, inicio, meio) 
+        mergeSort(lista, meio, fim) 
+        merge(lista, inicio, meio, fim)
+
+def merge(lista, inicio, meio, fim):
+    left = lista[inicio: meio]
+    rigth = lista[meio: fim]
+    head_left = 0
+    head_rigth = 0
+
+    size_l =  meio -  inicio
+    size_r = fim - meio
+
+    for k in range(inicio, fim):
+        if head_left >= size_l:
+            lista[k] = rigth[head_rigth]
+            head_rigth += 1
+
+        elif head_rigth >= size_r:
+            lista[k] = left[head_left]
+            head_left += 1
+        elif left[head_left][2] > rigth[head_rigth][2]:
+            
+            lista[k] = left[head_left]
+            head_left += 1
+        elif left[head_left][2] < rigth[head_rigth][2]:
+            print('entrou')
+            lista[k] = rigth[head_rigth]
+            head_rigth += 1
+
+                    
 
 
     
@@ -99,6 +154,30 @@ def main(args) -> None:
     acha_indices(arquivo)
     primeiraParteConteiners = primeiraParte(arquivo)
     segundaParteConteiners = segundaParte(arquivo)
+
+    dados = ordenaPeloCNPJEextraipercentual(primeiraParteConteiners, segundaParteConteiners)
+
+    listaAtualizada = dados[0]
+    inicio, fim = dados[1] , dados[1] + dados[2]
+
+    print(listaAtualizada)
+    print(inicio)
+    print(fim)
+    print(listaAtualizada)
+    mergeSort(listaAtualizada, inicio, fim)
+    print(listaAtualizada)
+
+    arquivoSaida = open('saida.txt', 'w')
+    for indiceDado in range(fim):
+        if indiceDado >= inicio:
+            conteiner = listaAtualizada[indiceDado]
+            arquivoSaida.write(f'{conteiner[0]} {conteiner[3]}Kg ({conteiner[2]}%)\n')
+
+        else:
+            print(listaAtualizada[indiceDado])
+           
+
+        
     #
     #fechando arquivos
     golden_input.close()
